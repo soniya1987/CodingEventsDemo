51f587a474113ef15ced72a7d0de54bebdc53fc8
Soniya Vakkat Kuttan <vksoniya68@gmail.com>
2022-12-06 22:01:57 -0600

    Added EventCategory implementation

--- CodingEventsDemo/Controllers/EventCategoryController.cs
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using CodingEventsDemo.Data;
 using CodingEventsDemo.Models;
+using CodingEventsDemo.Viewmodels;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +20,27 @@ namespace CodingEventsDemo.Controllers
             ViewBag.title = "All Categories";
             List<EventCategory> categories = context.Categories.ToList();
             return View(categories);
+        }
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel addEventCategoryViewModel = new AddEventCategoryViewModel();
+            return View(addEventCategoryViewModel);
+        }
+
+        [HttpPost]
+        [Route("EventCategory/Create")]
+        public  IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory category = new EventCategory();
+                category.Name = addEventCategoryViewModel.Name;
+
+                context.Categories.Add(category);
+                context.SaveChanges();
+                return Redirect("/EventCategory");
+            }
+            return View("EventCategory/Create", addEventCategoryViewModel);
         }
     }
 }
---
+++ CodingEventsDemo/Viewmodels/AddEventCategoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodingEventsDemo.Viewmodels
+{
+    public class AddEventCategoryViewModel
+    {
+        [Required(ErrorMessage ="Event category name is required")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Event category name should be between 3 and 20 characters long")]
+        public string Name { get; set; }
+
+        public AddEventCategoryViewModel()
+        {
+
+        }
+
+
+    }
+}
